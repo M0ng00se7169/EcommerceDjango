5ebac69bad1f6fbd05ff20a18bf3684d72d56556
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-11-03 23:41:44 +0200

    5. Added Billing Profile in Checkout View

---
+++ myApp/billing/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+
+
+User = settings.AUTH_USER_MODEL
+
+
+class BillingProfile(models.Model):
+    user      = models.ForeignKey(User, null=True, blank=True, on_delete=False, unique=True)
+    email     = models.EmailField()
+    active    = models.BooleanField(default=True)
+    update    = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        BillingProfile.objects.get_or_create(user=instance)
+
+
+post_save.connect(user_created_receiver, sender=User)
