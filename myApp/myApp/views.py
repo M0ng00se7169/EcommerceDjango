76258324f7738925c2a8e1d495e28b8a4011e29b
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-11-07 01:48:55 +0200

    3. Ajaxify the Contact Form

--- myApp/myApp/views.py
+++ myApp/myApp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 
 from .forms import ContactForm
@@ -16,11 +16,17 @@ def contact_page(request):
         "content": "Welcome to the contact page",
         "form": contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
+    # if request.method == "POST":
+    #     print(request.POST)
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission"})
 
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
     return render(request, "contact/view.html", context)
 
 
